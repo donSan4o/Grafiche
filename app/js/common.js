b3e57646a7bfc713912edb8429fb76c4cf7350bd
Alex Pogrebnjak <cah4o.pan4o@gmail.com>
2018-01-13 01:22:39 +0200

    add_product_page

--- app/js/common.js
+++ app/js/common.js
@@ -2,10 +2,27 @@ $(document).ready(function () {
     var swiper = new Swiper('.swiper-container', {
         paginationClickable: true,
         spaceBetween: 0,
-        speed: 1000,
+        speed: 1300,
         loop: true,
         autoplay: {
-            delay: 4000,
+            delay: 5000,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        autoplayDisableOnInteraction: false,
+        slidesPerView: 1,
+        autoHeight: true,
+        spaceBetween: 20
+    });
+    var swiper = new Swiper('.also-like-slider .swiper-container', {
+        paginationClickable: true,
+        spaceBetween: 0,
+        speed: 1300,
+        loop: true,
+        autoplay: {
+            delay: 5000,
         },
         navigation: {
             nextEl: '.swiper-button-next',
