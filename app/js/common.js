460f9c319fbe85bf07cfc20e23789c1f40454f42
Alex Pogrebnjak <cah4o.pan4o@gmail.com>
2018-01-19 17:45:04 +0200

    fix_all_sliders

--- app/js/common.js
+++ app/js/common.js
@@ -14,68 +14,50 @@ $(document).ready(function () {
         }
       ]
     });
-
-    // var swiper = new Swiper('.other-news-slider .swiper-container', {
-    //     paginationClickable: true,
-    //     spaceBetween: 0,
-    //     speed: 1300,
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 5000,
-    //     },
-    //     navigation: {
-    //         nextEl: '.other-news-slider .swiper-button-next',
-    //         prevEl: '.other-news-slider .swiper-button-prev',
-    //       },
-    //     autoplayDisableOnInteraction: false,
-    //     slidesPerView: 2,
-    //     autoHeight: true,
-    //     breakpoints: {
-    //         540: {
-    //           slidesPerView: 1,
-    //         }
-    //     }
-    // });
+    $('.also-like-slider .also-like-slider-inner').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    $('.blog-slider .blog-slider-holder').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
     
-    // var swiper = new Swiper('.blog-slider .swiper-container', {
-    //     paginationClickable: true,
-    //     spaceBetween: 0,
-    //     speed: 1200,
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 5000,
-    //     },
-    //     navigation: {
-    //         nextEl: '.blog-slider .swiper-button-next',
-    //         prevEl: '.blog-slider .swiper-button-prev',
-    //       },
-    //     autoplayDisableOnInteraction: false,
-    //     slidesPerView: 1,
-    //     autoHeight: true,
-    //     spaceBetween: 20
-    // });
-    // var swiper = new Swiper('.also-like-slider .swiper-container', {
-    //     paginationClickable: true,
-    //     speed: 1300,
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 5000,
-    //     },
-    //     navigation: {
-    //         nextEl: '.also-like-slider .swiper-button-next',
-    //         prevEl: '.also-like-slider .swiper-button-prev',
-    //       },
-    //     autoplayDisableOnInteraction: false,
-    //     slidesPerView: 4,
-    //     autoHeight: true,
-    //     spaceBetween: 0,
-    //     breakpoints: {
-    //         540: {
-    //           slidesPerView: 2,
-    //           spaceBetween: 0
-    //         }
-    //     }
-    // });
+    $('.news-slider-holder').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        ]
+    });
     $('.btn-carello').click(function () {
         $('.basket').show();
         $('.fader').show();
